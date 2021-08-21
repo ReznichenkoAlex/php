ac809e874aaf0eae97950eae65842be3ec3841d6
Alexey Reznichenko <lexcooprer1@gmail.com>
2021-08-21 11:00:54 +1000

    сделал второе дз

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once "src/functions.php";
+
+task1(['dasd', 'ewqe', 'czxvczxv', "<br>"]);
+
+task2("*", 2, 3, 2, 2);
+
+task3(10, 11);
+
+task4();
+
+task5();
+
+task6();
+
+task7();
+
+task8();
+
+task9('test.txt');
---
+++ src/functions.php
@@ -0,0 +1,125 @@
+<?php
+
+function task1($array, $flag = false)
+{
+    if ($flag) {
+        return implode($array);
+    }
+    foreach ($array as $v) {
+        echo "<p>" . $v;
+    }
+}
+
+function task2($sign, ...$numbers)
+{
+    $result = $numbers[0];
+    $resultString = "Результат: " . $result . " " . $sign . " ";
+    switch ($sign) {
+        case "+":
+            for ($i = 1; $i < sizeof($numbers); $i++) {
+                $result += $numbers[$i];
+                if ($i === sizeof($numbers) - 1) {
+                    $resultString .= $numbers[$i] . " ";
+                } else {
+                    $resultString .= $numbers[$i] . " $sign ";
+                }
+            }
+            $resultString .= " = " . $result;
+            echo $resultString;
+            break;
+        case "-":
+            for ($i = 1; $i < sizeof($numbers); $i++) {
+                $result -= $numbers[$i];
+                if ($i === sizeof($numbers) - 1) {
+                    $resultString .= $numbers[$i] . " ";
+                } else {
+                    $resultString .= $numbers[$i] . " $sign ";
+                }
+            }
+            $resultString .= " = " . $result;
+            echo $resultString;
+            break;
+        case "*":
+            for ($i = 1; $i < sizeof($numbers); $i++) {
+                $result *= $numbers[$i];
+                if ($i === sizeof($numbers) - 1) {
+                    $resultString .= $numbers[$i] . " ";
+                } else {
+                    $resultString .= $numbers[$i] . " $sign ";
+                }
+            }
+            $resultString .= " = " . $result;
+            echo $resultString;
+            break;
+        case "/":
+            for ($i = 1; $i < sizeof($numbers); $i++) {
+                $result /= $numbers[$i];
+                if ($i === sizeof($numbers) - 1) {
+                    $resultString .= $numbers[$i] . " ";
+                } else {
+                    $resultString .= $numbers[$i] . " $sign ";
+                }
+            }
+            $resultString .= " = " . $result;
+            echo $resultString;
+            break;
+        default:
+            echo "Введен неккоректный знак";
+            break;
+    }
+}
+
+function task3(...$numbers)
+{
+    if (sizeof($numbers) !== 2) {
+        echo "Введите 2 аргумента для функции";
+        return;
+    }
+    echo "<table>";
+    for ($i = 1; $i <= $numbers[0]; $i++) {
+        echo "<tr>";
+        for ($j = 1; $j <= $numbers[1]; $j++) {
+            echo "<td align='center'>";
+            echo $i * $j;
+            echo "</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+
+function task4()
+{
+    echo date("d.m.Y H:i") . "<br>";
+}
+
+function task5()
+{
+    echo date("d.m.Y H:i:s", time()) . "<br>";
+}
+
+function task6()
+{
+    $str = "Карл у Клары украл Кораллы<br>";
+    echo str_replace("К", "", $str);
+}
+
+function task7()
+{
+    $str = "Две бутылки лимонада<br>";
+    echo str_replace("Две", "Три", $str);
+}
+
+function task8()
+{
+    $fp = fopen('test.txt', 'a');
+    file_put_contents('test.txt', 'Hello again!');
+    fclose($fp);
+    echo "task8 отработала<br>";
+}
+
+function task9($name)
+{
+    echo file_get_contents($name);
+}
+
